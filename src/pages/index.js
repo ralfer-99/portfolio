014ce52443c8b1e94ced50b564773d78f366dbc8
Alfer <89336044+ralfer-99@users.noreply.github.com>
2025-04-14 10:01:14 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import Layout from '@/components/Layout'
 import Head from 'next/head'
 import Image from 'next/image'
-import profilePic from './public/images/profile/alf.png'
+import profilePic from '../../public/images/profile/alf.png'
 import AnimatedText from '@/components/AnimatedText'
 import { LinkArrow } from '@/components/Icons'
 import HireMe from '@/components/HireMe'
-import lightBulb from './public/images/svgs/miscellaneous_icons_1.svg'
+import lightBulb from '../../public/images/svgs/miscellaneous_icons_1.svg'
 import TransitionEffect from '@/components/TransitionEffect'
 
 export default function Home() {
